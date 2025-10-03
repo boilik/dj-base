f21ca5dd36b9eea1bef6f4cda5356ef7605d1545
boilik <qvouuge@gmail.com>
2025-10-04 05:20:41 +0700

    main edited

--- shop/settings.py
+++ shop/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'main', 
+    'main.apps.MainConfig',
 ]
 
 ROOT_URLCONF = 'shop.urls'
